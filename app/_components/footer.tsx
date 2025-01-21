fe5e4ed766d30806065be6d46128d46c9fd708ca
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2025-01-21 18:40:02 -0300

    style: update style text

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -2,10 +2,10 @@ const Footer = () => {
   return (
     <footer className="bg-green-900 p-4">
       <div className="max-w-6xl flex justify-between items-center flex-col gap-2 mx-auto md:flex-row">
-        <p className="text-white text-sm">
+        <p className="text-white text-center text-sm">
           ANDRE N. DE CARVALHO E CASTRO LTDA ® 45.645.354/0001-21 • © 2024
         </p>
-        <p className="text-white text-sm font-semibold">
+        <p className="text-white text-center text-sm font-semibold">
           Desenvolvido por Dlack Tecnologia
         </p>
       </div>
