f28057563152c52698c39797f8c048749044f49b
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2025-01-23 08:42:19 -0300

    feat: add meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Header from "./_components/header";
 import Footer from "./_components/footer";
 import IconWhatsApp from "./_components/icon-whatsapp";
+import Script from "next/script";
+import Image from "next/image";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -23,6 +25,45 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth" lang="pt-br">
+      {/* Google Tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-7L9F6SJEPC"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-7L9F6SJEPC');
+        `}
+      </Script>
+
+      {/* Meta Pixel Code */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1213243747181658');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <Image
+          alt=""
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1213243747181658&ev=PageView&noscript=1"
+        />
+      </noscript>
+
       <body className={poppins.className}>
         <IconWhatsApp />
         <Header />
