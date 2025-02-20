96a125db1fcb035070f1dd08d18d7da49b2f8197
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2025-02-20 20:37:49 -0300

    fix: update scripts tags to component layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "./_components/header";
 import Footer from "./_components/footer";
 import IconWhatsApp from "./_components/icon-whatsapp";
-import Script from "next/script";
-import Image from "next/image";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -25,46 +24,30 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth" lang="pt-br">
-      {/* Google Tag (gtag.js) */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-7L9F6SJEPC"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
+      {/* Google Tag Manager */}
+      <Script id="gtm-init" strategy="afterInteractive">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-7L9F6SJEPC');
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T8N5BMN7');
         `}
       </Script>
-
-      {/* Meta Pixel Code */}
-      <Script id="meta-pixel" strategy="afterInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1213243747181658');
-          fbq('track', 'PageView');
-        `}
-      </Script>
-      <noscript>
-        <Image
-          alt=""
-          height="1"
-          width="1"
-          style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=1213243747181658&ev=PageView&noscript=1"
-        />
-      </noscript>
+      {/* End Google Tag Manager */}
 
       <body className={poppins.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T8N5BMN7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <IconWhatsApp />
         <Header />
         {children}
